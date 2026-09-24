f28425b8b3f206d5ab69be53ae97036022cc5389
Noxstar256 <deejaynoxofficial@gmail.com>
2026-09-24 18:03:28 +0300

    Delete Commands/tool directory

--- Commands/tool/id.js
+++
@@ -1,2 +0,0 @@
-const config = require("../../config");
-module.exports={name:"id",aliases:["chatid"],category:"Tools",description:"Show the current chat ID",async execute({reply,from}){return reply(`🆔 Chat ID:\n${from}\n\n> ${config.BOT_NAME}\n> Powered by ${config.CREATOR}`);}};
--- Commands/tool/jid.js
+++
@@ -1,2 +0,0 @@
-const config = require("../../config");
-module.exports={name:"jid",aliases:["myjid"],category:"Tools",description:"Show the current chat ID",async execute({reply,from}){return reply(`🆔 Chat ID:\n${from}\n\n> ${config.BOT_NAME}\n> Powered by ${config.CREATOR}`);}};
